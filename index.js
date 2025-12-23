646ea628b29d36e59f8bb2af79383556ca42286f
sevennai7 <lesdawn456@gmail.com>
2025-12-23 00:23:43 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,19 +1,14 @@
-let index = 0; // Index
-
 fetch("data.json")
   .then(res => res.json())
   .then(data => {
+    const list = document.getElementById("list");
 
-    function showItem(i) {
-      const item = data[i];
-      document.getElementById("thing-name").textContent = item.n;
-      document.getElementById("thing-temp").textContent =
-        `${item.f}°F (${item.c}°C)`; // Get HTML and json thingy and etc 
-    }
-
-    showItem(index);
-    document.addEventListener("click", () => {
-      index = (index + 1) % data.length;
-      showItem(index);
+    data.forEach(item => {
+      const block = document.createElement("div");
+      block.innerHTML = `
+        <h1>${item.n}</h1>
+        <h2>${item.f}°F (${item.c}°C)</h2>
+      `;
+      list.appendChild(block);
     });
   });
